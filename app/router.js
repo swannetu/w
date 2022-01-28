8e5938d122a459adf65240328ac94439ff4134a1
Swann <22002727@unicaen.fr>
2022-01-28 12:02:32 +0100

    v1

--- app/router.js
+++ app/router.js
@@ -1,9 +1,12 @@
 import EmberRouter from '@ember/routing/router';
 import config from 'tds/config/environment';
 
+
 export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  this.route('ex1');
+});
