0d5269406e46eb297d2b515f9f672ae38c72e50e
Swann <22002727@unicaen.fr>
2022-02-02 14:16:15 +0100

    v2

---
+++ app/controllers/ex1.js
@@ -0,0 +1,42 @@
+import Controller from '@ember/controller';
+import {tracked} from '@glimmer/tracking';
+import {action} from '@ember/object';
+
+export default class Ex1Controller extends Controller {
+  MAX = 100;
+  @tracked content = 'Texte par defaut';
+  VAL;
+
+  get size() {
+    return this.MAX - this.content.length;
+  }
+
+  get style() {
+    if (this.size < 20) {
+      return 'Attention';
+    }
+    if (this.size < 50) {
+      return 'warning';
+    } else {
+      return 'info';
+    }
+  }
+
+  get modif() {
+    if (this.size < 20) {
+      return 'Attention';
+    } else {
+
+    }
+  }
+
+  @action
+  enregistrer() {
+    this.VAL = this.content;
+  }
+
+  @action
+  effacer() {
+    this.content = '';
+  }
+}
